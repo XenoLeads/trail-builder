913bc3b5baa70af15abb4177307f1620dea34c9e
Xeno <226741508+XenoLeads@users.noreply.github.com>
2025-10-12 21:22:52 +0530

    feat: code ui

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 import "../styles/style.css";
+
+const canvas = document.getElementsByClassName("canvas")[0];
+const ctx = canvas.getContext("2d");
+
+let canvas_size;
+
+function init() {
+  set_canvas_size();
+  clear_canvas();
+  window.onresize = set_canvas_size;
+}
+
+function set_canvas_size() {
+  canvas_size = get_canvas_size();
+  canvas.width = canvas_size;
+  canvas.height = canvas_size;
+  function get_canvas_size(margin = 7 / 10) {
+    const is_landscape = window.innerHeight < window.innerWidth;
+    if (is_landscape) return window.innerHeight * margin;
+    return window.innerWidth * margin;
+  }
+}
+
+function clear_canvas() {
+  ctx.fillStyle = "#1C1C1C";
+  ctx.fillRect(0, 0, canvas_size, canvas_size);
+}
+
+init();
